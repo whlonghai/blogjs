c477f73bdcb0aa60385b35e93b6764935d6ac5d6
whlonghai <whlonghai@gmail.com>
2020-09-09 12:42:12 +0700

    Create coqa.js

---
+++ coqa.js
@@ -0,0 +1,27 @@
+qa=[];q=[];a=[];
+function checkqa(){
+	if(document.getElementsByClassName("mt-0")[0].getElementsByTagName("pre")[0]==undefined) 
+	for (tq=0;tq<document.getElementsByClassName("mt-0").length;tq++){
+		q[tq]=document.getElementsByClassName("mt-0")[tq]
+		if(qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()]!=undefined){
+		a[tq]=q[tq].parentElement.parentElement.getElementsByClassName("question-option")
+			for (i=0;i<a[tq].length;i++){
+				for(j=0;j<qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()].length;j++){
+				   if(a[tq][i].innerText.replace(/(\n)|\s/gm, "").toLowerCase()==qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()][j])a[tq][i].style.color="red";
+				}
+			}
+		}
+	}else
+	for (tq=0;tq<document.getElementsByClassName("mt-0").length;tq++){
+	   q[tq]=document.getElementsByClassName("mt-0")[tq].getElementsByTagName("pre")[0]
+		if(qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()]!=undefined){
+		a[tq]=q[tq].parentElement.parentElement.parentElement.getElementsByClassName("question-option")
+			for (i=0;i<a[tq].length;i++){
+				for(j=0;j<qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()].length;j++){
+				   if(a[tq][i].innerText.replace(/(\n)|\s/gm, "").toLowerCase()==qa[q[tq].innerText.replace(/(\n)|\s/gm, "").toLowerCase()][j])a[tq][i].style.color="red";
+				}
+			}
+		}
+	}
+}
+setTimeout(function() {checkqa()}, 5000);
